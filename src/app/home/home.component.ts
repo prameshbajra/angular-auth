5516defe8a52576cf52dca76bb13529b4cc98c0f
Pramesh Bajracharya <pramesh.bajracharya@telqtele.com>
2022-07-06 19:52:11 +0545

    Adding random stuff here ...

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+    message: string = 'You are not logged in';
 
-  ngOnInit(): void {
-  }
+    constructor(private http: HttpClient) { }
+
+    ngOnInit(): void {
+        // This does not work ...
+        // Backend needs Authorization header with Bearer token ... Not only cookie ...
+        this.http.get('http://localhost:8000/api/user', { withCredentials: true }).subscribe(response => {
+            console.log(response);
+        }, (err => {
+            console.log(err);
+        }));
+    }
 
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-login',
@@ -13,7 +14,8 @@ export class LoginComponent implements OnInit {
 
     constructor(
         private formBuilder: FormBuilder,
-        private http: HttpClient
+        private http: HttpClient,
+        private router: Router
     ) { }
 
     ngOnInit(): void {
@@ -24,7 +26,11 @@ export class LoginComponent implements OnInit {
     }
 
     submit(): void {
-        console.log(this.loginForm.getRawValue());
+        this.http.post('http://localhost:8000/api/login', this.loginForm.getRawValue(), { withCredentials: true })
+            .subscribe(response => {
+                console.log(response);
+                this.router.navigate(['/']);
+            });
     }
 
 }
